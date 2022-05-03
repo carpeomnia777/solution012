7d859cc52463032d973773b58ad764a4465ced4f
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-05-03 19:11:36 -0400

    sum of positive array codewars -completed

---
+++ sumOfPosArray.js
@@ -0,0 +1,17 @@
+function positiveSum(arr) {
+  
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i]> 0){
+            sum += arr[1]
+        }
+    //   if(Math.sign(i)==1){
+    //     sum += arr[i];
+    //     }
+    //   else if(Math.sign(i)==-1){
+    //     i =0;
+    //   }
+    }
+
+    return sum;
+}
